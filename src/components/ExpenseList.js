b53e3c141bc1fad252357d8dfe86b1a9d6c3a792
s.zaghdoudi <sofiene.zaghdoudi@gmail.com>
2024-01-07 22:27:46 +0100

    [Feat3] Update Expense List

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,31 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import ExpenseItem from './ExpenseItem'
 
 const ExpenseList = () => {
+
+    const {expenses} = useContext(AppContext);
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th scope='col'>Items</th>
+                    <th scope='col'>Quantity</th>
+                    <th scope='col'>Unit Price</th>
+                    <th scope='col'>Item Price</th>
+                    <th scope='col'>Remove</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((expense)=>(
+                    <ExpenseItem id={expense.id} key={expense.id} name={expense.name} quantity={expense.quantity} unitprice={expense.unitprice}/>
+                ))}
+            </tbody>    
+        </table>
+    );   
+
+
 };
 
 export default ExpenseList;
